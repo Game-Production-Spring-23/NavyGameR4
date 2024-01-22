c8bbf5ccd9ac6e8ff06ccc607c8250aa080e8b5d
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-01-22 17:25:01 -0500

    init commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+let textX = 50;
+let textY = 0;
+textDown = true;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    textSize(50);
+    if (textY >= 300) {
+        textDown = false
+    }
+
+    if (textY <= 0) {
+        textDown = true
+    }
+
+    if (textDown) {
+        textY++
+    } else {
+        textY--
+    }
+
+    text('p5 works', textX, textY);
+}
