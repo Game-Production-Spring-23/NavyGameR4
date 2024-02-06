16094b3a7e67a47db3682e9ee81c02828129f957
AndrewOfIndana <acbay612@gmail.com>
2024-02-06 01:26:44 -0500

    Added Scene Transitions

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,11 +1,13 @@
 extends Node2D
 @onready var gameController = get_tree().root.get_node("/root/GameController")
+@onready var sceneTransition = get_tree().root.get_node("/root/SceneTransition")
 #poor implementation ^
 
 @export var sceneName : String
 
 func _on_start_button_pressed():
-	gameController.changeScene("res://scenes/" + sceneName + ".tscn")
+	sceneTransition.loadScene(sceneName)
+	# gameController.changeScene("res://scenes/" + sceneName + ".tscn")
 
 func _on_end_button_pressed():
 	get_tree().quit()	
