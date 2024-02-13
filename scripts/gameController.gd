08f15cf188fa67e57908864bb070702b844befa6
zdicken <zdicken@iu.edu>
2024-02-13 02:50:35 -0500

    Update gameController.gd
    forgot i was explicitly told to add detailed comments and just pushed because it made sense to me
    -made the comments actually usefulish to read
    -also changed the audio playback in the changeMusic() function to call new function playMusic() so other scripts can actually pause and play music if need be (for cutscenes or dramatic effect etc.)
    -made sure that the intro and body don't start playing at the same time with a maybe-necessary pauseMusic() call. whether it's needed is based on whether audio player nodes are paused upon changing streams

--- scripts/gameController.gd
+++ scripts/gameController.gd
@@ -4,23 +4,30 @@ var textSpeed = 1
 @onready var animationPlayer = $ScreenTransition/FadeScreen/AnimationPlayer
 
 func changeScene(scenePath, intro = null, body = null):
-	#play the fade animation
+	#play the fade out animation, then switch scene and fade back in
 	animationPlayer.play("Fade")
 	await animationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Levels/" + scenePath + ".tscn")
 	animationPlayer.play_backwards("Fade")
-	#change music
+	#checks if the changeScene() call provides both music paths, plays it if yes and simply pauses it if not
 	if(intro and body):
 		changeMusic(intro, body)
 	else:
 		pauseMusic()
 
+#takes two resource paths, sets the music components to those and plays the intro portion again
 func changeMusic(intro, body):
+	#haven't actually tested to make sure the audio pauses when changed but just in case it doesn't
+	pauseMusic()
 	$music/intro.stream = intro
 	$music/body.stream = body
+	playMusic()
+
+#slightly inelegant - possible to take the playback time when it is paused and pass it back in to start at the time it was paused
+func playMusic():
 	$music/intro.play()
-	
+
+#sets both the intro and body music players to pause
 func pauseMusic():
-	#sets both the intro and body to pause
 	$music/intro.stream_paused = true
 	$music/body.stream_paused = true
