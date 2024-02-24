c5303ed7f226e5adc7266a5c642c5273cca686dc
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-02-24 12:24:58 -0500

    Speed
    Gotta go fast

--- characters/charPlaceholder.gd
+++ characters/charPlaceholder.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 #parameters/idle/blend_position
 
-@export var move_speed : float = 100
+@export var move_speed : float = 500
 @export var starting_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
