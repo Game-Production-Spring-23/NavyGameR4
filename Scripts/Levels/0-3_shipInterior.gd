d77596fae1b625169de2279baefeae7402d3368e
AndrewOfIndana <acbay612@gmail.com>
2024-02-25 00:31:08 -0500

    Added 0-3 Level

---
+++ Scripts/Levels/0-3_shipInterior.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+"""
+	name: 0-3_shipInterior.gd
+	description: This script controls the level managment of the ship's interior
+
+"""
+# SETUP FUNCTIONS
+# Called when the node enters the scene tree for the first time.
+# func _ready():
+
+func _on_area_2d_body_entered(body):
+    if body.name == "Player":
+        gameController.changeScene("testEnd")
