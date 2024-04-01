666bb277851754d3e23056b230f7d4889a68a9f1
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-04-01 14:15:46 -0400

    minigame 2 should work

--- scripts/levels/2-3_minigame2.gd
+++ scripts/levels/2-3_minigame2.gd
@@ -1,13 +1,14 @@
 extends Node2D
-@onready var player = $player
-@onready var alien = $alien
+
+@export var travin: Sprite2D
+
 """
 	name: 2-3_minigame2.gd
 	description: This script manages minigame 2
 
 """
 
-var questions = [1,2,3,4,5,6,7]
+var questions = [1,2,3,4]
 var nextQuestion = 0 #the index of the current question
 var progession = 0
 
@@ -15,24 +16,22 @@ func _ready():
 	question()
 
 func question():
-	nextQuestion = randi_range(0, questions.size() - 1)
-	gameController.triggerDialogue("chapter1", "minigameQuestion" + str(questions[nextQuestion]))
+	gameController.triggerDialogue("chapter2", "minigameQuestion" + str(questions[progession]))
 
 func answer(correct):
 	if(correct):
-		questions.remove_at(nextQuestion)
-		if(progession < 8):
+		if(progession < 3):
+			travin.texture = load("res://assets/images/sprites/minigame_2/shopkeep_sprite_happy.png")
 			progession += 1
-			player.position.x += 100
-		if(questions.size() == 0):
-			gameController.changeScene("1-4_dialogue", "res://assets/audio/music/swamp-intro.ogg", "res://assets/audio/music/swamp-body.ogg")
-		else:
 			await get_tree().create_timer(2.0).timeout
 			question()
+		else:
+			#CHANGE ONCE PROPER SCENE IS ADDED vvv
+			gameController.changeScene("1-4_dialogue", "res://assets/audio/music/swamp-intro.ogg", "res://assets/audio/music/swamp-body.ogg")
 	else:
 		if(progession > 0):
 			progession -= 1
-			player.position.x -= 100
+		travin.texture = load("res://assets/images/sprites/minigame_2/shopkeep_sprite_mad.png")
 		await get_tree().create_timer(2.0).timeout
 		question()
 
