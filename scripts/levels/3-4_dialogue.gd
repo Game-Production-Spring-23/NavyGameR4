79b99df47fe3dde395ab3f532cbb8c7fdccdd87e
jago-1009 <garwoodj22@gmail.com>
2024-04-09 14:52:31 -0400

    Updated minigame splash screens.

--- scripts/levels/3-4_dialogue.gd
+++ scripts/levels/3-4_dialogue.gd
@@ -1,6 +1,6 @@
 extends Node2D
 func _ready():
-	gameController.triggerDialogue("chapter3", "chapter3_5")
+	gameController.triggerDialogue("chapter3", "chapter3_4")
 	
 func endDialogue3():
 	gameController.changeScene("3-5_review", "res://assets/audio/music/menu-theme-intro.ogg", "res://assets/audio/music/menu-theme-body.ogg")
