469675ec4efc4ea58eef0239ddd035e9c40af469
jago-1009 <garwoodj22@gmail.com>
2024-04-15 19:09:50 -0400

    added OceanSplash

---
+++ scripts/levels/5-0_oceanSplash.gd
@@ -0,0 +1,23 @@
+extends Node2D
+@onready var animationPlayer = $ScreenTransition/FadeScreen/AnimationPlayer
+@export var textureRect: Node
+@export var timer: Node
+
+"""
+	name: swampSplash.gd
+	description: This script manages the swamp splash screen and loads the swamp level after
+
+"""
+
+#The timer automatically starts. When it's done, transition to the next slide and run the timer again. 
+func _on_timer_timeout():
+	##CHANGE ONCE OCEAN IS FINISHED
+	gameController.changeScene("4-1_desertLevel", "res://assets/audio/music/swamp-intro.ogg", "res://assets/audio/music/swamp-body.ogg")
+
+#Skips intro cutscene when button pressed	
+## CHANGE WHEN OCEAN IS FINISHED	
+func _on_button_pressed():
+	gameController.playSound("res://assets/audio/sfx/gui-confirmation.ogg")
+	gameController.changeScene("4-0_desertSplash.tscn", "res://assets/audio/music/swamp-intro.ogg", "res://assets/audio/music/swamp-body.ogg")
+		
+	
