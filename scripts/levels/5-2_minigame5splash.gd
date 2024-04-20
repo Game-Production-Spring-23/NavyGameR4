70cdc220addef970e74a118c42da0ab97f68e43d
jago-1009 <garwoodj22@gmail.com>
2024-04-20 13:34:56 -0400

    Cafe Level Complete.

--- scripts/levels/5-2_minigame5splash.gd
+++ scripts/levels/5-2_minigame5splash.gd
@@ -5,15 +5,15 @@ extends Node2D
 
 """
 	name: 1-2_minigame1splash.gd
-	description: This script manages the minigame splash screen and loads the swamp level after
+	description: This script manages the minigame splash screen and loads Minigame 5
 
 """
 
 #The timer automatically starts. When it's done, transition to the next slide and run the timer again. 
 func _on_timer_timeout():
-	gameController.changeScene("6-3_minigame6", null, "res://assets/audio/music/minigame.ogg")
+	gameController.changeScene("5-3_minigame5", null, "res://assets/audio/music/minigame.ogg")
 
 #Skips intro cutscene when button pressed		
 func _on_button_pressed():
 	gameController.playSound("res://assets/audio/sfx/gui-confirmation.ogg")
-	gameController.changeScene("6-3_minigame6", null, "res://assets/audio/music/minigame.ogg")
+	gameController.changeScene("5-3_minigame5", null, "res://assets/audio/music/minigame.ogg")
--- scripts/levels/6-1_cityLevel.gd
+++ scripts/levels/6-1_cityLevel.gd
@@ -7,16 +7,8 @@ func _on_interact_NPC_1_trigger_player_interact():
 	gameController.triggerDialogue("chapter6","chapter6_NPC_3")
 
 
-func _on_interact_NPC_2_trigger_player_interact():
-	gameController.triggerDialogue("chapter6","chapter6_NPC_2")
 
 
-func _on_interact_NPC_3_trigger_player_interact():
-	gameController.triggerDialogue("chapter6","chapter6_NPC_1")
-
-
-func _on_interact_Yosie_trigger_player_interact():
-	gameController.triggerDialogue("chapter6","chapter6_3")
 
 
 func _on_area_trigger_player_interact():
