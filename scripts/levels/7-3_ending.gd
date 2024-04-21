2adcdbd335211c8079f39c615adc08a0dc9aebf7
AndrewOfIndana <99208998+AndrewOfIndana@users.noreply.github.com>
2024-04-20 23:21:14 -0400

    Update 7-3_ending.gd

--- scripts/levels/7-3_ending.gd
+++ scripts/levels/7-3_ending.gd
@@ -4,6 +4,6 @@ name: 7-3_ending.gd
 description: This script manages the ending cutscene
 
 """
-. 
+ 
 func _on_timer_timeout():
 	gameController.changeScene("7-4_credits", "res://assets/audio/music/menu-theme-intro.ogg", "res://assets/audio/music/menu-theme-body.ogg")
