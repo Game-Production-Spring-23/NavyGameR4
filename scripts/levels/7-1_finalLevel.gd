5a33d7bf977b312c04abb0ee111b6a853cb9b319
AndrewOfIndana <99208998+AndrewOfIndana@users.noreply.github.com>
2024-04-21 18:13:03 -0400

    Small bug fixes for release

--- scripts/levels/7-1_finalLevel.gd
+++ scripts/levels/7-1_finalLevel.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	gameController.characterCanMove = true
+
 
 func _on_area_trigger_player_interact():
 	gameController.changeScene("7-2_dialogue", "res://assets/audio/music/swamp-intro.ogg", "res://assets/audio/music/swamp-body.ogg")
